26c4f7e46f55378b0f547282b0c073c318ba7557
Jordin Brouwer <git@jordinbrouwer.nl>
2021-02-22 18:31:00 +0100

    Added support to enable and disable the addon using the interface options (#1)

---
+++ AutomaticRoleCheck_Panel.lua
@@ -0,0 +1,27 @@
+AutomaticRoleCheck.Panel = CreateFrame("Frame", nil, UIParent)
+AutomaticRoleCheck.Panel:RegisterEvent("ADDON_LOADED")
+AutomaticRoleCheck.Panel:RegisterEvent("PLAYER_LOGOUT")
+AutomaticRoleCheck.Panel.name = "AutomaticRoleCheck"
+
+AutomaticRoleCheck.Panel.Inner = AutomaticRoleCheck.Panel:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
+AutomaticRoleCheck.Panel.Inner:SetPoint("TOPLEFT", 10, -15)
+AutomaticRoleCheck.Panel.Inner:SetJustifyH("LEFT")
+AutomaticRoleCheck.Panel.Inner:SetJustifyV("TOP")
+AutomaticRoleCheck.Panel.Inner:SetText("AutomaticRoleCheck")
+
+AutomaticRoleCheck.Panel.Inner.EnabledButton = CreateFrame("CheckButton", "EnabledButton", AutomaticRoleCheck.Panel, "ChatConfigCheckButtonTemplate")
+AutomaticRoleCheck.Panel.Inner.EnabledButton:SetPoint("TOPLEFT", 10, -45)
+AutomaticRoleCheck.Panel.Inner.EnabledButton.Text:SetFontObject("GameFontHighlightSmall")
+AutomaticRoleCheck.Panel.Inner.EnabledButton.Text:SetPoint("LEFT", AutomaticRoleCheck.Panel.Inner.EnabledButton, "RIGHT", 0, 1)
+AutomaticRoleCheck.Panel.Inner.EnabledButton.Text:SetText("Enable AutomaticRoleCheck")
+AutomaticRoleCheck.Panel.Inner.EnabledButton.tooltip = AutomaticRoleCheck.Panel.Inner.EnabledButton.Text:GetText()
+
+AutomaticRoleCheck.Panel.Inner.EnabledButton:HookScript("OnClick", function()
+    AutomaticRoleCheck.Options.Enabled = AutomaticRoleCheck.Panel.Inner.EnabledButton:GetChecked()
+end)
+AutomaticRoleCheck.Panel:HookScript("OnShow", function()
+    AutomaticRoleCheck.Panel.Inner.EnabledButton:SetChecked(AutomaticRoleCheck.Options.Enabled)
+end)
+AutomaticRoleCheck.Panel:HookScript("OnEvent", AutomaticRoleCheck.EventHandler)
+
+InterfaceOptions_AddCategory(AutomaticRoleCheck.Panel)
